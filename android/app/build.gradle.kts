ee5f14deae6cbf71ea4f21f91dc62725e4847ebc
lhnguyenVP21 <lhnguyen21@vp.fitus.edu.vn>
2025-02-27 11:45:03 +0700

    dependency test

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.advancedmobile_chatai"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
