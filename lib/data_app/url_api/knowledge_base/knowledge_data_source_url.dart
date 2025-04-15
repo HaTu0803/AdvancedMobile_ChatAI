b4057d52ebdec240c996fd1fad5247138ed33c66
HaTu0803 <htttu0803@gmail.com>
2025-04-16 01:32:03 +0700

    update remote knowledge base

--- lib/data_app/url_api/knowledge_base/knowledge_data_source_url.dart
+++ lib/data_app/url_api/knowledge_base/knowledge_data_source_url.dart
@@ -1,12 +1,1 @@
-import 'package:advancedmobile_chatai/data_base/url_api/root_url.dart';
 
-const String baseUrl = '$KNOWLEDGE_BASE_URL/knowledge';
-
-class ApiKnowledgeBaseUrl {
-  static const String createKnowledge = baseUrl;
-  static const String getKnowledges = baseUrl;
-  static String updateKnowledge(String id) => '$baseUrl/$id';
-  static String deleteKnowledge(String id) => '$baseUrl/$id';
-
-  static String getUnitsOfKnowledge(String id) => '$baseUrl/$id/units';
-}
