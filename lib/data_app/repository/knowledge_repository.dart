75a85a2a57f2fbf2cb8fef1df4db565000ac872a
HaTu0803 <htttu0803@gmail.com>
2025-04-16 09:05:43 +0700

    repository knowledge

---
+++ lib/data_app/repository/knowledge_repository.dart
@@ -0,0 +1,60 @@
+import 'package:advancedmobile_chatai/data_app/model/base/base_model.dart';
+import 'package:advancedmobile_chatai/data_app/model/knowledge_base/knowledge_model.dart';
+import 'package:advancedmobile_chatai/data_app/remote/knowledge_base/knowledge_remote.dart';
+import 'package:flutter/material.dart';
+
+class KnowledgeRepository {
+  final KnowledgeApiClient KnowledgeApi = KnowledgeApiClient();
+
+  Future<KnowledgeResponse> createKnowledge(Knowledge request) async {
+    try {
+      final response = await KnowledgeApi.createKnowledge(request);
+      return response;
+    } catch (e) {
+      debugPrint("GetConversations Error: $e");
+      rethrow;
+    }
+  }
+
+  Future<KnowledgeListResponse> getKnowledges(BaseQueryParams params) async {
+    try {
+      final response = await KnowledgeApi.getKnowledges(params);
+      return response;
+    } catch (e) {
+      debugPrint("GetConversations Error: $e");
+      rethrow;
+    }
+  }
+
+  Future<KnowledgeListResponse> updateKnowledge(
+      String id, Knowledge request) async {
+    try {
+      final response = await KnowledgeApi.updateKnowledge(id, request);
+      return response;
+    } catch (e) {
+      debugPrint("GetConversations Error: $e");
+      rethrow;
+    }
+  }
+
+  Future<bool> deleteKnowledge(String id) async {
+    try {
+      final response = await KnowledgeApi.deleteKnowledge(id);
+      return response;
+    } catch (e) {
+      debugPrint("GetConversations Error: $e");
+      rethrow;
+    }
+  }
+
+  Future<UnitsOfKnowledgeListResponse> getUnitsOfKnowledge(
+      BaseQueryParams params, String id) async {
+    try {
+      final response = await KnowledgeApi.getUnitsOfKnowledge(params, id);
+      return response;
+    } catch (e) {
+      debugPrint("GetConversations Error: $e");
+      rethrow;
+    }
+  }
+}
