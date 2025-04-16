963577ce1348ab70d41a74c73f3672f661d303ac
HaTu0803 <htttu0803@gmail.com>
2025-04-16 11:17:18 +0700

    update remote subscribe

---
+++ lib/data_app/remote/jarvis/subscription_remote.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+
+import 'package:advancedmobile_chatai/core/config/api_headers.dart';
+import 'package:advancedmobile_chatai/core/helpers/dialog_helper.dart';
+import 'package:advancedmobile_chatai/core/helpers/refresh_token_helper.dart';
+import 'package:advancedmobile_chatai/core/local_storage/base_preferences.dart';
+import 'package:advancedmobile_chatai/data_app/model/jarvis/subscription_model.dart';
+import 'package:advancedmobile_chatai/data_app/url_api/jarvis/subcription_url.dart';
+import 'package:http/http.dart' as http;
+
+class SubscriptionApiClient {
+  Future<UsageResponse> getUsage() async {
+    await BasePreferences.init();
+    String token = await BasePreferences().getTokenPreferred('access_token');
+    print("🔑 AccessToken: $token");
+
+    final url = Uri.parse(ApiJarvisSubscriptionUrl.getUsage);
+    final headers = ApiHeaders.getAIChatHeaders("", token);
+
+    final response = await http.get(url, headers: headers);
+
+    print("📩 response.statusCode: ${response.statusCode}");
+    print("📩 response.body: ${response.body}");
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return UsageResponse.fromJson(jsonDecode(response.body)['data']);
+    } else if (response.statusCode == 401) {
+      final retryResponse = await retryWithRefreshToken(
+        url: url,
+        headers: headers,
+        body: null,
+      );
+
+      if (retryResponse.statusCode == 200 || retryResponse.statusCode == 201) {
+        return UsageResponse.fromJson(jsonDecode(retryResponse.body)['data']);
+      } else {
+        DialogHelper.showError(
+            'Phiên đăng nhập hết hạn. Vui lòng đăng nhập lại.');
+        throw Exception('Phiên đăng nhập hết hạn. Vui lòng đăng nhập lại.');
+      }
+    } else {
+      DialogHelper.showError('Lỗi: ${response.statusCode}');
+      throw Exception('Lỗi: ${response.statusCode}');
+    }
+  }
+
+  Future<bool> subscribe() async {
+    await BasePreferences.init();
+    String token = await BasePreferences().getTokenPreferred('access_token');
+    print("🔑 AccessToken: $token");
+
+    final url = Uri.parse(ApiJarvisSubscriptionUrl.getUsage);
+    final headers = ApiHeaders.getAIChatHeaders("", token);
+
+    final response = await http.get(url, headers: headers);
+
+    print("📩 response.statusCode: ${response.statusCode}");
+    print("📩 response.body: ${response.body}");
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return true;
+    } else if (response.statusCode == 401) {
+      final retryResponse = await retryWithRefreshToken(
+        url: url,
+        headers: headers,
+        body: null,
+      );
+
+      if (retryResponse.statusCode == 200 || retryResponse.statusCode == 201) {
+        return true;
+      } else {
+        DialogHelper.showError(
+            'Phiên đăng nhập hết hạn. Vui lòng đăng nhập lại.');
+        throw Exception('Phiên đăng nhập hết hạn. Vui lòng đăng nhập lại.');
+      }
+    } else {
+      DialogHelper.showError('Lỗi: ${response.statusCode}');
+      throw Exception('Lỗi: ${response.statusCode}');
+    }
+  }
+}
